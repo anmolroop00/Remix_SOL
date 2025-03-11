13e2505e9cf7aa3f765c02d9b1a5ec01bbe01fd4
anmolroop00 <anmol.roop00@gmail.com>
2025-03-11 22:52:36 +0530

    Update the Get and Set Function for stateVariable

--- contracts/lab4.sol
+++ contracts/lab4.sol
@@ -5,5 +5,11 @@ pragma solidity ^0.8.7;
 contract demo{
     int stVar;
 
-    function set() public 
+    function set(int _value) public{
+        stVar = _value;
+    }
+
+    function get() public view returns(int){
+        return stVar;
+    }
 }
